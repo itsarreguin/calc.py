58f8b42cddefa6e2afefa9a825c4a717528d6a26
Brandon Arreguin <arreguin.tech@gmail.com>
2022-01-19 20:41:30 -0600

    Modified main code

--- calc.py
+++ calc.py
@@ -21,47 +21,60 @@ Choose an option: """
 
     try:
         option = int(input(menu))
-        message = 'The result is:'
+        message = 'The result of'
+
 
         if option > 0 and option < 8:
+
             if option > 0 and option < 5:
-                num_x = int(input('Enter a first number: '))
-                num_y = int(input('Enter a second number: '))
+                num1 = int(input('Enter a first number: '))
+                num2 = int(input('Enter a second number: '))
 
                 if option == 1:
-                    print(f'{message} {num_x + num_y}')
+                    print(f'{message} {num1} + {num2} is: {num1 + num2}')
+
                 elif option == 2:
-                    print(f'{message} {num_x - num_y}')
+                    print(f'{message} {num1} - {num2} is: {num1 - num2}')
+
                 elif option == 3:
-                    print(f'{message} {num_x * num_y}')
+                    print(f'{message} {num1} x {num2} is: {num1 * num2}')
+
                 elif option == 4:
                     try:
-                        print(f'{message} {num_x / num_y}')
+                        print(f'{message} {num1} / {num2} is: {num1 / num2}')
                     except ZeroDivisionError:
-                        print(' '), clear()
-                        print("You can't divide between zero")
+                        clear()
+
+                        print("\n You can't divide between zero")
                         main()
 
             if option > 4 and option < 7:
                 if option == 5:
-                    main_num = int(input('Enter a base to get the power: '))
+                    base = int(input('Enter a base to get the power: '))
                     power = int(input('Exponent to get power: '))
-                    print(f'{message} {main_num**power}')
+
+                    print(f'{message} {base}**{power} is: {base**power}')
 
                 elif option == 6:
                     sqr_num = int(input('Enter a number to get square root: '))
-                    print(f'{message} {math.sqrt(sqr_num)}')
+
+                    print(f'{message} {sqr_num} is: {math.sqrt(sqr_num)}')
+
             if option == 7:
+                clear()
                 print('Closed program')
         else:
-            print(' '), clear()
-            print('Invalid option')
+            clear()
+
+            print('n\ Invalid option')
             main()
 
-    except ValueError:
-        print(' '), clear()
-        print('Only can introduce numbers')
+    except ValueError as ve:
+        clear()
+
+        print('n\ Only can introduce numbers')
         return main()
+
 
 if __name__ == '__main__':
     main()
